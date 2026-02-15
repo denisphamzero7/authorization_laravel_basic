6c79c4fdd7ebfe3a61fa79817fe7ea33ad7d285f
denisphamzero7 <pnhau2001@gmail.com>
2026-02-15 21:53:42 +0700

    dang post

--- app/Http/Controllers/Admin/PostsController.php
+++ app/Http/Controllers/Admin/PostsController.php
@@ -4,24 +4,44 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
     //
 
     public function index(){
-        $lists = Post::all();
+        $lists = Post::latest()->get();
         return view('admin.posts.list',compact('lists'));
     }
 
     public function add(){
-        return "Thêm bài viết";
+        return view('admin.posts.add');
     }
-    public function postadd(){
-        return "Thêm bài viết";
+    public function postadd(Request $request){
+         $request -> validate(
+                [
+                    'title' => 'required|string',
+                    'content' => 'required|string',
+
+                ],[
+                    'title.required' => 'Trường :attribute bắt buộc phải nhập',
+                    'title.string' => 'Trường :attribute không hợp lệ',
+                    'content.required' => 'Trường :attribute bắt buộc phải nhập',
+                    'content.string' =>'Trường :attribute không hợp lệ',
+
+                ]
+            );
+         $post = new Post();
+         $post->title=$request->title;
+         $post->content=$request->content;
+         $post->user_id= Auth::user()->id;
+         $post->save();
+         return redirect()->route('admin.posts.index')->with('msg','thêm thành công');
+
     }
-    public function edit(){
-        return "Thêm bài viết";
+    public function edit(Request $request, Post $post){
+        return view('admin.posts.edit', compact('post'));
     }
     public function postedit(){
         return "Thêm bài viết";
